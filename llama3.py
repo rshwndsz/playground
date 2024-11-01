e1c26fe60a083eadafbbcf21038b95239ed41273
Russel Dsouza <rshwndsz@gmail.com>
2024-11-01 18:24:27 +0000

    [WIP] More changes to simplify & identify bug

--- llama3.py
+++ llama3.py
@@ -4,8 +4,7 @@ from dataclasses import dataclass
 
 import jax
 import jax.numpy as jnp
-import jax.random as jrn
-from einops import rearrange, repeat
+from einops import rearrange
 from safetensors.flax import load_file
 from transformers import AutoTokenizer
 
@@ -13,6 +12,7 @@ from transformers import AutoTokenizer
 # Meta: https://github.com/meta-llama/llama-models/blob/main/models/llama3/reference_impl/model.py
 # Unsloth: https://github.com/unslothai/unsloth/blob/main/unsloth/models/llama.py
 # Huggingface: https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py
+# Sebastian Raschka: https://github.com/rasbt/LLMs-from-scratch/blob/main/ch05/07_gpt_to_llama/standalone-llama32.ipynb
 
 
 @dataclass
@@ -20,7 +20,6 @@ class Params:
     v: int
     n: int
     d: int
-    d_ff: int
     h: int
     h_kv: int
     max_seq_len: int
@@ -36,11 +35,11 @@ class Params:
 
 def rms_norm(x, gamma, eps):
     # https://arxiv.org/pdf/1910.07467
-    return gamma * x * jax.lax.rsqrt(jnp.mean(jnp.pow(x, 2), axis=-1, keepdims=True) + eps)
+    return gamma * x * jax.lax.rsqrt(jnp.mean(jnp.square(x), axis=-1, keepdims=True) + eps)
 
 
 def swish(x, beta: float):
-    return x * jax.lax.logistic(beta * x)
+    return x * jax.nn.sigmoid(beta * x)
 
 
 def ffn(x, W1, V, W2):
@@ -51,84 +50,71 @@ def ffn(x, W1, V, W2):
     return x
 
 
-def rope_scaling(f):
-    # Weird scaling added in from grid search
-    scale_factor = 32.0
-    high_freq_factor = 4.0
-    low_freq_factor = 1.0
-    og_ctx_len = 8192
-
-    # Get wavelengths from frequencies
-    wl = 2 * jnp.pi / f
-
-    # Conditions on wavelength
-    is_low_wl = wl < (og_ctx_len / low_freq_factor)
-    is_high_wl = wl > (og_ctx_len / high_freq_factor)
-    is_bound_wl = ~(is_low_wl | is_high_wl)
-
-    # Scale frequencies based on conditions
-    scaled_f = f.clone()
-    scaled_f[is_high_wl] = f[is_high_wl] / scale_factor
-    if is_bound_wl.any():
-        smooth = (og_ctx_len / wl[is_bound_wl] - low_freq_factor) / (high_freq_factor - low_freq_factor)
-        scaled_f[is_bound_wl] = (1 - smooth) * f[is_bound_wl] / scale_factor + smooth * f[is_bound_wl]
-    return scaled_f
-
-
-def rope_frequencies(dim: int, end: int, theta: float, scaling: bool = False):
+def rope_frequencies(dim, ctx_len, theta, use_scaling=False):
     # https://arxiv.org/pdf/2104.09864
-    m = jnp.arange(end)
-    t = 1.0 / (theta ** (jnp.arange(0, dim, 2)[: dim // 2] / dim))
-    if scaling:
+    def rope_scaling(f):
+        # Weird scaling added in from grid search
+        scale_factor = 32.0
+        high_freq_factor = 4.0
+        low_freq_factor = 1.0
+        og_ctx_len = 8192
+
+        # Get wavelengths from frequencies
+        wl = 2 * jnp.pi / f
+
+        # Conditions on wavelength
+        is_low_wl = wl < (og_ctx_len / low_freq_factor)
+        is_high_wl = wl > (og_ctx_len / high_freq_factor)
+        is_bound_wl = ~(is_low_wl | is_high_wl)
+
+        # Scale frequencies based on conditions
+        scaled_f = jnp.copy(f)
+        scaled_f[is_high_wl] = f[is_high_wl] / scale_factor
+        if is_bound_wl.any():
+            smooth = (og_ctx_len / wl[is_bound_wl] - low_freq_factor) / (high_freq_factor - low_freq_factor)
+            scaled_f[is_bound_wl] = (1 - smooth) * f[is_bound_wl] / scale_factor + smooth * f[is_bound_wl]
+        return scaled_f
+
+    m = jnp.arange(ctx_len, dtype=jnp.float32)
+    t = 1.0 / (theta ** (jnp.arange(0, dim, 2, dtype=jnp.float32)[: (dim // 2)] / dim))
+    if use_scaling:
         t = rope_scaling(t)
     f = jnp.einsum("i, j -> ij", m, t)
-    return jnp.cos(f) + 1j * jnp.sin(f)
+
+    f = jnp.concatenate([f, f], axis=-1)
+    return jnp.cos(f), jnp.sin(f)
 
 
-def rope(x, f_complex):
-    x = x.reshape(*x.shape[:-1], -1, 2).astype(jnp.float32)
-    x_complex = jax.lax.complex(x[..., 0], x[..., 1])
-    f_complex = jnp.expand_dims(f_complex, 1)
-
-    x_rotated = x_complex * f_complex
-    x_out = jnp.stack([x_rotated.real, x_rotated.imag], axis=-1)
-    x_out = rearrange(x_out, "s head k complex -> s head (k complex)")
-    return x_out
+def rope(x, f_cos, f_sin):
+    x1, x2 = jnp.split(x, 2, axis=-1)
+    rotated = jnp.concatenate([-x2, x1], axis=-1)
+    x_rotated = x * f_cos + rotated * f_sin
+    return x_rotated
 
 
-def gqa(x, h, h_kv, W_Q, W_K, W_V, W_O, f_complex):
+def gqa(x, h, h_kv, W_Q, W_K, W_V, W_O, f_cos, f_sin):
     # https://arxiv.org/pdf/2305.13245v3
-    _, d = x.shape
-
     # Project
     Q = jnp.einsum("ik, jk -> ij", x, W_Q)
     K = jnp.einsum("ik, jk -> ij", x, W_K)
     V = jnp.einsum("ik, jk -> ij", x, W_V)
 
     # Split along head
-    Q = rearrange(Q, "s (head head_dim) -> s head head_dim", head=h)
-    K = rearrange(K, "s (head head_dim) -> s head head_dim", head=h_kv)
-    V = rearrange(V, "s (head head_dim) -> s head head_dim", head=h_kv)
+    Q = rearrange(Q, "s (head head_dim) -> head s head_dim", head=h)
+    K = rearrange(K, "s (head head_dim) -> head s head_dim", head=h_kv)
+    V = rearrange(V, "s (head head_dim) -> head s head_dim", head=h_kv)
 
     # Apply rotary embeddings
-    Q = rope(Q, f_complex)
-    K = rope(K, f_complex)
-
-    # KV Cache
-    # TODO
-
-    # Rearrange to parallelise attention computation across head
-    Q = rearrange(Q, "s head head_dim -> head s head_dim")
-    K = rearrange(K, "s head head_dim -> head s head_dim")
-    V = rearrange(V, "s head head_dim -> head s head_dim")
+    Q = rope(Q, f_cos, f_sin)
+    K = rope(K, f_cos, f_sin)
 
     # Grouped Query / Repeated Key-Value
-    K = repeat(K, "head_kv s head_dim -> (head_kv n_rep) s head_dim", n_rep=h // h_kv)
-    V = repeat(V, "head_kv s head_dim -> (head_kv n_rep) s head_dim", n_rep=h // h_kv)
+    K = jnp.repeat(K, h // h_kv, axis=0)
+    V = jnp.repeat(V, h // h_kv, axis=0)
 
     # Query-Key Lookup
     scores = jnp.einsum("hik, hjk -> hij", Q, K)
-    scores /= jnp.sqrt(d // h)
+    scores /= jnp.sqrt(K.shape[-1])
 
     # Causal mask
     mask = jnp.triu(jnp.ones_like(scores), k=1) * (-1e8)
@@ -146,29 +132,37 @@ def gqa(x, h, h_kv, W_Q, W_K, W_V, W_O, f_complex):
     return out
 
 
-def xfmr(tokens, w, params, f_complex, pos):
+def xfmr(tokens, w, params, f_cos, f_sin, pos):
     s = len(tokens)
 
     x = w["model.embed_tokens.weight"][jnp.array(tokens)]
-    f_complex = f_complex[pos : pos + s]
+    f_cos, f_sin = f_cos[pos : pos + s], f_sin[pos : pos + s]
 
     for i in range(params.n):
-        x += gqa(
-            rms_norm(x, w[f"model.layers.{i}.input_layernorm.weight"], eps=params.norm_eps),
+        r = x
+        x = rms_norm(x, w[f"model.layers.{i}.input_layernorm.weight"], eps=params.norm_eps)
+        x = gqa(
+            x,
+            h=params.h,
+            h_kv=params.h_kv,
             W_Q=w[f"model.layers.{i}.self_attn.q_proj.weight"],
             W_K=w[f"model.layers.{i}.self_attn.k_proj.weight"],
             W_V=w[f"model.layers.{i}.self_attn.v_proj.weight"],
             W_O=w[f"model.layers.{i}.self_attn.o_proj.weight"],
-            h=params.h,
-            h_kv=params.h_kv,
-            f_complex=f_complex,
+            f_cos=f_cos,
+            f_sin=f_sin,
         )
-        x += ffn(
-            rms_norm(x, w[f"model.layers.{i}.post_attention_layernorm.weight"], eps=params.norm_eps),
+        x += r
+
+        r = x
+        x = rms_norm(x, w[f"model.layers.{i}.post_attention_layernorm.weight"], eps=params.norm_eps)
+        x = ffn(
+            x,
             W1=w[f"model.layers.{i}.mlp.up_proj.weight"],
             W2=w[f"model.layers.{i}.mlp.down_proj.weight"],
             V=w[f"model.layers.{i}.mlp.gate_proj.weight"],
         )
+        x += r
 
     # Llama models have a final norm
     x = rms_norm(x, w["model.norm.weight"], eps=params.norm_eps)
@@ -180,11 +174,13 @@ def xfmr(tokens, w, params, f_complex, pos):
     return logits[-1, :]
 
 
-def sample(logits, key):
+def sample(logits, key, temperature=0.6):
+    if temperature != 0:
+        logits /= temperature
     probs = jax.nn.softmax(logits, axis=-1)
     topk_probs, topk_indices = jax.lax.top_k(probs, 50)
     topk_probs /= jnp.sum(topk_probs)
-    idx = jrn.choice(key, topk_indices, p=topk_probs)
+    idx = jax.random.choice(key, topk_indices, p=topk_probs)
     return idx
 
 
@@ -213,12 +209,11 @@ def main(args):
         v=128256,
         n=16,
         d=2048,
-        d_ff=8192,
         h=32,
         h_kv=8,
         max_seq_len=2048,
         norm_eps=1e-5,
-        rope_theta=500000.0,
+        rope_theta=500_000.0,
         rope_scaling=False,
         rope_scale_factor=32,
         rope_high_freq_factor=4.0,
@@ -226,18 +221,18 @@ def main(args):
     )
 
     # Precompute frequences for 2048*2 = 4096
-    frequencies = rope_frequencies(
+    f_cos, f_sin = rope_frequencies(
         dim=params.d // params.h,
-        end=params.max_seq_len * 2,
+        ctx_len=params.max_seq_len * 2,
         theta=params.rope_theta,
-        scaling=params.rope_scaling,
+        use_scaling=params.rope_scaling,
     )
 
     # Generate
-    key = jrn.key(args.key)
+    key = jax.random.PRNGKey(args.key)
     for pos in range(args.len):
-        _, key = jrn.split(key)
-        logits = xfmr(tokens, weights, params, f_complex=frequencies, pos=pos)
+        _, key = jax.random.split(key)
+        logits = xfmr(tokens, weights, params, f_cos, f_sin, pos=pos)
         sampled = sample(logits, key)
         gen = tokenizer.decode([sampled])
         print(gen, end="", flush=True)
