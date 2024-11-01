[WIP] Add rope scaling + fixes to rope

llama3.py
@@ -9,6 +9,11 @@ from einops import rearrange, repeat
 from safetensors.flax import load_file
 from transformers import AutoTokenizer
 
+# References
+# Meta: https://github.com/meta-llama/llama-models/blob/main/models/llama3/reference_impl/model.py
+# Unsloth: https://github.com/unslothai/unsloth/blob/main/unsloth/models/llama.py
+# Huggingface: https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py
+
 
 @dataclass
 class Params:
@@ -18,16 +23,19 @@ class Params:
     d_ff: int
     h: int
     h_kv: int
+    max_seq_len: int
+
+    norm_eps: float
 
     rope_theta: float
-    rope_factor: float
+    rope_scaling: bool
+    rope_scale_factor: float
     rope_high_freq_factor: float
     rope_low_freq_factor: float
 
 
-def rms_norm(x, gamma, eps=1e-8, axis=0):
+def rms_norm(x, gamma, eps, axis=0):
     # https://arxiv.org/pdf/1910.07467
-    # https://github.com/meta-llama/llama-models/blob/2fe1a1690162910660332e3294a552cf0ec7e754/models/llama3/reference_impl/model.py#L31-L42
     return x * gamma / jnp.sqrt(jnp.mean(jnp.pow(x, 2), axis=axis, keepdims=True) + eps)
 
 
@@ -37,35 +45,58 @@ def swish(x, beta: float):
 
 def ffn(x, W1, V, W2):
     # https://arxiv.org/pdf/2002.05202v1
-    # https://github.com/meta-llama/llama-models/blob/2fe1a1690162910660332e3294a552cf0ec7e754/models/llama3/reference_impl/model.py#L218-L244
     # Pytorch's nn.Linear is equivalent to jnp.dot(x, W.T)
     x = swish(jnp.dot(x, W1.T), beta=1.0) * jnp.dot(x, V.T)
     x = jnp.dot(x, W2.T)
     return x
 
 
-def compute_rope_frequencies(dim: int, end: int, theta: float):
-    # https://github.com/meta-llama/llama-models/blob/2fe1a1690162910660332e3294a552cf0ec7e754/models/llama3/reference_impl/model.py#L45-L100
+def rope_scaling(f):
+    # Weird scaling added in from grid search
+    scale_factor = 32.0
+    high_freq_factor = 4.0
+    low_freq_factor = 1.0
+    og_ctx_len = 8192
+
+    wl = 2 * jnp.pi / f
+
+    is_low_wl = wl < (og_ctx_len / low_freq_factor)
+    is_high_wl = wl > (og_ctx_len / high_freq_factor)
+    is_bound_wl = ~(is_low_wl | is_high_wl)
+
+    scaled_f = f.clone()
+    scaled_f[is_high_wl] = f[is_high_wl] / scale_factor
+
+    if is_bound_wl.any():
+        smooth = (og_ctx_len / wl[is_bound_wl] - low_freq_factor) / (high_freq_factor - low_freq_factor)
+        scaled_f[is_bound_wl] = (1 - smooth) * f[is_bound_wl] / scale_factor + smooth * f[is_bound_wl]
+
+    return scaled_f
+
+
+def rope_frequencies(dim: int, end: int, theta: float, scaling: bool = False):
     # https://arxiv.org/pdf/2104.09864
     m = jnp.arange(end)
     t = jnp.pow(theta, -jnp.arange(0, dim, 2)[: dim // 2] / dim)
+    if scaling:
+        t = rope_scaling(t)
     f = jnp.einsum("i, j -> ij", m, t)
     return jnp.cos(f) + 1j * jnp.sin(f)
 
 
 def rope(x, f):
-    x_complex = x[..., ::2] + 1j * x[..., 1::2]
+    _x = x.reshape(*x.shape[:-1], -1, 2).astype(jnp.float32)
+    x_complex = jax.lax.complex(_x[..., 0], _x[..., 1])
+    # >>> NOTE
+    # assert f.shape == (x_complex.shape[1], x_complex.shape[-1])
     x_rotated = x_complex * f
-
-    x_out = jnp.zeros_like(x)
-    x_out.at[..., 0::2].set(x_rotated.real.astype(x))
-    x_out.at[..., 1::2].set(x_rotated.imag.astype(x))
+    x_out = jnp.stack([x_rotated.real, x_rotated.imag], axis=-1)
+    x_out = rearrange(x_out, "head s k complex -> head s (k complex)")
     return x_out
 
 
 def gqa(x, h, h_kv, W_Q, W_K, W_V, W_O, f):
     # https://arxiv.org/pdf/2305.13245v3
-    # https://github.com/meta-llama/llama-models/blob/2fe1a1690162910660332e3294a552cf0ec7e754/models/llama3/reference_impl/model.py#L103-L215
     _, d = x.shape
 
     # Project
@@ -105,18 +136,19 @@ def gqa(x, h, h_kv, W_Q, W_K, W_V, W_O, f):
     attention = rearrange(attention, "head s head_dim -> s (head head_dim)")
 
     # Output projection
-    # >>> NOTE Shapes
     out = jnp.einsum("ik, jk -> ij", attention, W_O)
     return out
 
 
-def xfmr(tokens, w, params):
-    # https://github.com/meta-llama/llama-models/blob/2fe1a1690162910660332e3294a552cf0ec7e754/models/llama3/reference_impl/model.py#L247-L334
+def xfmr(tokens, w, params, f, pos):
+    s = len(tokens)
+
     x = w["model.embed_tokens.weight"][jnp.array(tokens)]
-    f = compute_rope_frequencies(dim=params.d // params.h, end=len(tokens), theta=params.rope_theta)
+    f = f[pos : pos + s]
+
     for i in range(params.n):
         x += gqa(
-            rms_norm(x, w[f"model.layers.{i}.input_layernorm.weight"]),
+            rms_norm(x, w[f"model.layers.{i}.input_layernorm.weight"], eps=params.norm_eps),
             W_Q=w[f"model.layers.{i}.self_attn.q_proj.weight"],
             W_K=w[f"model.layers.{i}.self_attn.k_proj.weight"],
             W_V=w[f"model.layers.{i}.self_attn.v_proj.weight"],
@@ -126,14 +158,19 @@ def xfmr(tokens, w, params):
             f=f,
         )
         x += ffn(
-            rms_norm(x, w[f"model.layers.{i}.post_attention_layernorm.weight"]),
+            rms_norm(x, w[f"model.layers.{i}.post_attention_layernorm.weight"], eps=params.norm_eps),
             W1=w[f"model.layers.{i}.mlp.up_proj.weight"],
             W2=w[f"model.layers.{i}.mlp.down_proj.weight"],
             V=w[f"model.layers.{i}.mlp.gate_proj.weight"],
         )
 
-    x = rms_norm(x, w["model.norm.weight"])
+    # Llama models have a final norm
+    x = rms_norm(x, w["model.norm.weight"], eps=params.norm_eps)
+
+    # Use transpose of word embeddings matrix as tie_word_embeddings is true
+    # https://huggingface.co/meta-llama/Llama-3.2-1B-Instruct/blob/9213176726f574b556790deb65791e0c5aa438b6/config.json#L34
     logits = jnp.einsum("ij, jk -> ik", x, w["model.embed_tokens.weight"].T)
+
     return logits[-1, :]
 
 
@@ -146,21 +183,7 @@ def sample(logits, key):
 
 
 def main(args):
-    key = jrn.key(args.key)
-    # https://huggingface.co/meta-llama/Llama-3.2-1B-Instruct/blob/main/config.json
-    params = Params(
-        v=128256,
-        n=16,
-        d=2048,
-        d_ff=8192,
-        h=32,
-        h_kv=8,
-        rope_theta=500000.0,
-        rope_factor=32,
-        rope_high_freq_factor=4.0,
-        rope_low_freq_factor=1.0,
-    )
-
+    # Load weights & tokenizer from huggingface
     if not args.instruct:
         model_id = "meta-llama/Llama-3.2-1B"
         weights = load_file(os.path.join(args.weights, "Llama-3.2-1B.safetensors"))
@@ -178,11 +201,37 @@ def main(args):
             return_dict=True,
         )["input_ids"]
 
-    xfmr(tokens, weights, params)
+    # https://huggingface.co/meta-llama/Llama-3.2-1B-Instruct/blob/main/config.json
+    # https://github.com/meta-llama/llama-models/blob/main/models/llama3/api/args.py
+    params = Params(
+        v=128256,
+        n=16,
+        d=2048,
+        d_ff=8192,
+        h=32,
+        h_kv=8,
+        max_seq_len=2048,
+        norm_eps=1e-5,
+        rope_theta=500000.0,
+        rope_scaling=False,
+        rope_scale_factor=32,
+        rope_high_freq_factor=4.0,
+        rope_low_freq_factor=1.0,
+    )
 
-    for _ in range(args.len):
+    # Precompute frequences for 2048*2 = 4096
+    frequencies = rope_frequencies(
+        dim=params.d // params.h,
+        end=params.max_seq_len * 2,
+        theta=params.rope_theta,
+        scaling=params.rope_scaling,
+    )
+
+    # Generate
+    key = jrn.key(args.key)
+    for pos in range(args.len):
         _, key = jrn.split(key)
-        logits = xfmr(tokens, weights, params)
+        logits = xfmr(tokens, weights, params, f=frequencies, pos=pos)
         sampled = sample(logits, key)
         gen = tokenizer.decode([sampled])
         print(gen, end="", flush=True)